52c060d31d62ad618cbf5f2322b2f4ea0eea06a9
Rachel Fairchild <rfair009@gmail.com>
2023-03-15 17:27:23 -0400

    Get random villager shuffle button to work!

--- src/App.js
+++ src/App.js
@@ -2,23 +2,15 @@ import React, { Component } from 'react';
 import { useState } from 'react';
 import orangeImg from "./img/orange.png";
 
-// TODO:
-// Add villager images to squares
-//     + Random
-//     + Check for no repeats
-//     + Shuffle button?
-// Replace square 'X' markers with icon marker
 
-
-function Square({value, onSquareClick, onShuffleClick, onClick, shuffleBoard}) {
+function Square({value, background, onSquareClick}) {
   return (
     <div>
       <button 
         className="square" 
-        onClick={onSquareClick} 
-        //style={{ backgroundImage: `url("https://acnhapi.com/v1/images/villagers/"${onShuffleClick()}")` }}
-        // START HERE**************************
-        style={onClick}
+        onClick={onSquareClick}
+        data-url={background}
+        style={{backgroundImage: `url(${background})`}}
       >
         {value}
       </button>
@@ -26,32 +18,25 @@ function Square({value, onSquareClick, onShuffleClick, onClick, shuffleBoard}) {
   );
 }
 
-
-// Populate bingo card with villager images
-// let alreadyUsed = [];
-// function villagerPopulate() {
-//   console.log('function running!');
-//   let villagerNum = Math.floor(Math.random() * 391);
-//   while(alreadyUsed.includes(villagerNum)) {
-//     villagerNum = Math.floor(Math.random() * 391);
-//   }
-//   console.log(`villager number = ${villagerNum}`);
-//   alreadyUsed.push(villagerNum);
-//   const villagerUrl = `https://acnhapi.com/v1/images/villagers/${villagerNum}`; 
-//   let villagerStr = `backgroundImage: "url("${villagerUrl}")"`;
-//   return villagerStr;
-// }
-
+function Button({onButtonClick}) {
+    return (
+        <div>
+            <button 
+                className="shuffle" 
+                onClick={onButtonClick}
+            >
+                Shuffle!
+            </button>
+        </div>
+    );
+}
 
 export default function Board() {
-  // A piece of state in the Board component: establishes state for toggle clicking
-  // Board declares a state variable named squares that defaults to an array of 25 nulls corresponding to the 25 squares
   const [squares, setSquares] = useState(Array(25).fill(null));
-  // Establishes state for background img
   const [board, shuffleBoard] = useState(Array(25).fill(null));
 
   function handleClick(i) {
-    // // Create a copy of the squares array and stores in the variable 'nextSquares'
+    console.log('handleclick running!')
     const nextSquares = squares.slice();
     if(!squares[i]) {
       // nextSquares[i] = (
@@ -63,28 +48,24 @@ export default function Board() {
     } else {
       nextSquares[i] = null;
     }
-
-    // Let React know that the state of the component has changed, triggers a re-render of the components that use the squares state (Board) as well as child components (Square components that make up the board)
     setSquares(nextSquares);
   }
 
-  // let alreadyUsed = [];
   function populateImages() {
-    console.log('Im running')
-    // create copy of the board array, store in nextBoard
-    let nextBoard = board.slice();
+    console.log('populateImages running');
+    const nextBoard = board.slice();
+    let alreadyUsed = [];
     for(let i = 0; i < 25; i++) {
       let villagerNum = Math.floor(Math.random() * 391);
-      // Add alreadyUsd check later
-      let villagerUrl = `https://acnhapi.com/v1/images/villagers/${villagerNum}`;
-      //let styleString = 'backgroundImage: url("https://acnhapi.com/v1/images/villagers/' + villagerNum + '")';
-      let styleString = `backgroundImage: url("${villagerUrl}")`;
+      while(alreadyUsed.includes(villagerNum) || villagerNum < 1) {
+        villagerNum = Math.floor(Math.random() * 391);
+      }
+      alreadyUsed.push(villagerNum);
+      const villagerUrl = `https://acnhapi.com/v1/images/villagers/${villagerNum}`;
 
-      nextBoard[i] = styleString;
-      // return styleString;
+      nextBoard[i] = villagerUrl;
+      console.log(nextBoard[i]);
     }
-    console.log(nextBoard);
-
     shuffleBoard(nextBoard);
   }
   
@@ -97,46 +78,45 @@ export default function Board() {
   return (
     <div>
       <div className="board-row">
-        <Square value={squares[0]} style={ board } onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} style={ board } onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} style={ board } onSquareClick={() => handleClick(2)} />
-        <Square value={squares[3]} style={ board } onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} style={ board } onSquareClick={() => handleClick(4)} />
+        <Square value={squares[0]} background={board[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} background={board[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} background={board[2]} onSquareClick={() => handleClick(2)} />
+        <Square value={squares[3]} background={board[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} background={board[4]} onSquareClick={() => handleClick(4)} />
       </div>
       <div className="board-row">
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-        <Square value={squares[9]} onSquareClick={() => handleClick(9)} />
+        <Square value={squares[5]} background={board[5]} onSquareClick={() => handleClick(5)} />
+        <Square value={squares[6]} background={board[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} background={board[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} background={board[8]} onSquareClick={() => handleClick(8)} />
+        <Square value={squares[9]} background={board[9]} onSquareClick={() => handleClick(9)} />
       </div>
       <div className="board-row">
-        <Square value={squares[10]} onSquareClick={() => handleClick(10)} />
-        <Square value={squares[11]} onSquareClick={() => handleClick(11)} />
-        <Square value={squares[12]} onSquareClick={() => handleClick(12)} />
-        <Square value={squares[13]} onSquareClick={() => handleClick(13)} />
-        <Square value={squares[14]} onSquareClick={() => handleClick(14)} />
+        <Square value={squares[10]} background={board[10]} onSquareClick={() => handleClick(10)} />
+        <Square value={squares[11]} background={board[11]} onSquareClick={() => handleClick(11)} />
+        <Square value={squares[12]} background={board[12]} onSquareClick={() => handleClick(12)} />
+        <Square value={squares[13]} background={board[13]} onSquareClick={() => handleClick(13)} />
+        <Square value={squares[14]} background={board[14]} onSquareClick={() => handleClick(14)} />
       </div>
       <div className="board-row">
-        <Square value={squares[15]} onSquareClick={() => handleClick(15)} />
-        <Square value={squares[16]} onSquareClick={() => handleClick(16)} />
-        <Square value={squares[17]} onSquareClick={() => handleClick(17)} />
-        <Square value={squares[18]} onSquareClick={() => handleClick(18)} />
-        <Square value={squares[19]} onSquareClick={() => handleClick(19)} />
+        <Square value={squares[15]} background={board[15]} onSquareClick={() => handleClick(15)} />
+        <Square value={squares[16]} background={board[16]} onSquareClick={() => handleClick(16)} />
+        <Square value={squares[17]} background={board[17]} onSquareClick={() => handleClick(17)} />
+        <Square value={squares[18]} background={board[18]} onSquareClick={() => handleClick(18)} />
+        <Square value={squares[19]} background={board[19]} onSquareClick={() => handleClick(19)} />
       </div>
       <div className="board-row">
-        <Square value={squares[20]} onSquareClick={() => handleClick(20)} />
-        <Square value={squares[21]} onSquareClick={() => handleClick(21)} />
-        <Square value={squares[22]} onSquareClick={() => handleClick(22)} />
-        <Square value={squares[23]} onSquareClick={() => handleClick(23)} />
-        <Square value={squares[24]} onSquareClick={() => handleClick(24)} />
+        <Square value={squares[20]} background={board[20]} onSquareClick={() => handleClick(20)} />
+        <Square value={squares[21]} background={board[21]} onSquareClick={() => handleClick(21)} />
+        <Square value={squares[22]} background={board[22]} onSquareClick={() => handleClick(22)} />
+        <Square value={squares[23]} background={board[23]} onSquareClick={() => handleClick(23)} />
+        <Square value={squares[24]} background={board[24]} onSquareClick={() => handleClick(24)} />
       </div>
       <div className="status">{status}</div>
-      <button className="shuffle" value={board} onClick={() => populateImages()}>Shuffle</button>
+      <Button className="shuffle" onButtonClick={() => populateImages()}>Shuffle</Button>
     </div>
   );
 }
-
 
 function calculateWinner(squares) {
   const lines = [
