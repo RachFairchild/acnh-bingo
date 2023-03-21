157a35b290e7c4af83b32c2bdc959e934ff918e4
Rachel Fairchild <rfair009@gmail.com>
2023-03-21 15:34:11 -0400

    Implement feature to allow user to choose marker image

--- src/App.js
+++ src/App.js
@@ -1,49 +1,18 @@
 import React, { Component } from 'react';
 import { useState } from 'react';
-// import appleImg from "./img/apple.png";
-// import cherryImg from "./img/cherry.png";
-// import coconutImg from "./img/coconut.png";
+import appleImg from "./img/apple.png";
+import cherryImg from "./img/cherry.png";
+import coconutImg from "./img/coconut.png";
 import orangeImg from "./img/orange.png";
-// import peachImg from "./img/peach.png";
-// import pearImg from "./img/pear.png";
-
+import peachImg from "./img/peach.png";
+import pearImg from "./img/pear.png";
 
 // TODO:
-//   - Add option to choose marker
 //   - Make it purty
 
-function Marker(marker, selection) {
-  // console.log(`Marker component running: src = ${marker.selection}`);
-  // console.log(marker.selection)
-  // console.log(`backgroundImage: `url(${marker.selection})``);
+function Square({value, selection, title, background, onSquareClick}) {  
   return (
     <div>
-      <img 
-        className='markerImage'
-        // src={marker}
-        // style={{backgroundImage: `url(${marker.selection})`}}
-        // style={{backgroundImage: `${selection}`}}
-        src={selection}
-        alt='Bingo marker'
-        width='125px' 
-        height='125px' 
-      />
-    </div>
-  );
-}
-
-function Square({value, marker, squares, selection, title, background, onSquareClick, Board, Marker, markerPick}) {
-  //console.log(`Square component running: src = ${selection}`);
-  // marker variable isn't reaching this component for some reason
-  return (
-    <div>
-      {/* <button 
-        className='markerImage' 
-        style={{backgroundImage: `url(${orangeImg})`}}
-        alt='Bingo marker'
-        width='125px' 
-        height='125px' 
-      /> */}
       <button 
         className="square" 
         onClick={onSquareClick}
@@ -52,7 +21,7 @@ function Square({value, marker, squares, selection, title, background, onSquareC
       >
         <img 
           className="activeMarkers"
-          src={orangeImg}
+          src={selection}
           alt="BINGO marker" 
           style={{
             zIndex: 10,
@@ -63,7 +32,6 @@ function Square({value, marker, squares, selection, title, background, onSquareC
           width='125px'
           height='125px'
         />
-        {Marker}
         <h1 
           className="villagerName"
         >
@@ -91,51 +59,26 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(25).fill(null));
   const [board, shuffleBoard] = useState(Array(25).fill(null));
   const [name, setName] = useState(Array(25).fill(null));
-  const [marker, setMarker] = useState({orangeImg}); // `appleImg`
-
-  //console.log(`${marker.orangeImg} is the same as ${orangeImg}`);
+  const [marker, setMarker] = useState({appleImg}); // `appleImg`
 
   function markerPick(i) {
     console.log('markerPick is running...');
-
-    const options = ['appleImg', 'cherryImg', 'coconutImg', 'orangeImg', 'peachImg', 'pearImg'];
+    const options = [appleImg, cherryImg, coconutImg, orangeImg, peachImg, pearImg];
     let selection = options[i];
-    //ket selectionUrl = `./img/${selection}.png`;
-
     setMarker(selection);
-
-    // console.log(`User chose ${selection}`);
-    console.log(`Marker state is now ${selection}`);
+    console.log(`marker state is now ${selection}`);
   }
 
-  
   function handleClick(i) {
     console.log(`Square ${i} clicked!`)
     const nextSquares = squares.slice();
-    
     if(!squares[i]) {
-      // nextSquares[i] = (
-        // <img 
-          // src={marker}
-          // className='markerImage' 
-          // style={{backgroundImage: `url(${marker})`}}
-          // alt='Bingo marker'
-          // width='125px' 
-          // height='125px' 
-        // />
-      // );
       nextSquares[i] = true;
-      // nextMark[i] = (
-      //   <img src={marker} />
-      // );
     } else {
       nextSquares[i] = null;
-      // nextMark[i] = null;
     }
     setSquares(nextSquares);
-    // setMarker(nextMark);
-    }
-  
+  }
   
   function populateImages() {
     setSquares(Array(25).fill(null));
@@ -180,40 +123,40 @@ export default function Board() {
   return (
     <div>
       <div className="board-row">
-        <Square value={squares[0]} title={name[0]} background={board[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} title={name[1]} background={board[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} title={name[2]} background={board[2]} onSquareClick={() => handleClick(2)} />
-        <Square value={squares[3]} title={name[3]} background={board[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} title={name[4]} background={board[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[0]} title={name[0]} background={board[0]} selection={marker} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} title={name[1]} background={board[1]} selection={marker} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} title={name[2]} background={board[2]} selection={marker} onSquareClick={() => handleClick(2)} />
+        <Square value={squares[3]} title={name[3]} background={board[3]} selection={marker} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} title={name[4]} background={board[4]} selection={marker} onSquareClick={() => handleClick(4)} />
       </div>
       <div className="board-row">
-        <Square value={squares[5]} title={name[5]} background={board[5]} onSquareClick={() => handleClick(5)} />
-        <Square value={squares[6]} title={name[6]} background={board[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} title={name[7]} background={board[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} title={name[8]} background={board[8]} onSquareClick={() => handleClick(8)} />
-        <Square value={squares[9]} title={name[9]} background={board[9]} onSquareClick={() => handleClick(9)} />
+        <Square value={squares[5]} title={name[5]} background={board[5]} selection={marker} onSquareClick={() => handleClick(5)} />
+        <Square value={squares[6]} title={name[6]} background={board[6]} selection={marker} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} title={name[7]} background={board[7]} selection={marker} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} title={name[8]} background={board[8]} selection={marker} onSquareClick={() => handleClick(8)} />
+        <Square value={squares[9]} title={name[9]} background={board[9]} selection={marker} onSquareClick={() => handleClick(9)} />
       </div>
       <div className="board-row">
-        <Square value={squares[10]} title={name[10]} background={board[10]} onSquareClick={() => handleClick(10)} />
-        <Square value={squares[11]} title={name[11]} background={board[11]} onSquareClick={() => handleClick(11)} />
+        <Square value={squares[10]} title={name[10]} background={board[10]} selection={marker} onSquareClick={() => handleClick(10)} />
+        <Square value={squares[11]} title={name[11]} background={board[11]} selection={marker} onSquareClick={() => handleClick(11)} />
         {/* Free space: */}
-        <Square value={squares[12]} title="FREE" background={`https://i0.wp.com/mynintendonews.com/wp-content/uploads/2019/03/animal_crossing_tom_nook_bells.jpg?fit=960%2C960&ssl=1`} onSquareClick={() => handleClick(12)} />
-        <Square value={squares[13]} title={name[13]} background={board[13]} onSquareClick={() => handleClick(13)} />
-        <Square value={squares[14]} title={name[14]} background={board[14]} onSquareClick={() => handleClick(14)} />
+        <Square value={squares[12]} title="FREE" background={`https://i0.wp.com/mynintendonews.com/wp-content/uploads/2019/03/animal_crossing_tom_nook_bells.jpg?fit=960%2C960&ssl=1`} selection={marker} onSquareClick={() => handleClick(12)} />
+        <Square value={squares[13]} title={name[13]} background={board[13]} selection={marker} onSquareClick={() => handleClick(13)} />
+        <Square value={squares[14]} title={name[14]} background={board[14]} selection={marker} onSquareClick={() => handleClick(14)} />
       </div>
       <div className="board-row">
-        <Square value={squares[15]} title={name[15]} background={board[15]} onSquareClick={() => handleClick(15)} />
-        <Square value={squares[16]} title={name[16]} background={board[16]} onSquareClick={() => handleClick(16)} />
-        <Square value={squares[17]} title={name[17]} background={board[17]} onSquareClick={() => handleClick(17)} />
-        <Square value={squares[18]} title={name[18]} background={board[18]} onSquareClick={() => handleClick(18)} />
-        <Square value={squares[19]} title={name[19]} background={board[19]} onSquareClick={() => handleClick(19)} />
+        <Square value={squares[15]} title={name[15]} background={board[15]} selection={marker} onSquareClick={() => handleClick(15)} />
+        <Square value={squares[16]} title={name[16]} background={board[16]} selection={marker} onSquareClick={() => handleClick(16)} />
+        <Square value={squares[17]} title={name[17]} background={board[17]} selection={marker} onSquareClick={() => handleClick(17)} />
+        <Square value={squares[18]} title={name[18]} background={board[18]} selection={marker} onSquareClick={() => handleClick(18)} />
+        <Square value={squares[19]} title={name[19]} background={board[19]} selection={marker} onSquareClick={() => handleClick(19)} />
       </div>
       <div className="board-row">
-        <Square value={squares[20]} title={name[20]} background={board[20]} onSquareClick={() => handleClick(20)} />
-        <Square value={squares[21]} title={name[21]} background={board[21]} onSquareClick={() => handleClick(21)} />
-        <Square value={squares[22]} title={name[22]} background={board[22]} onSquareClick={() => handleClick(22)} />
-        <Square value={squares[23]} title={name[23]} background={board[23]} onSquareClick={() => handleClick(23)} />
-        <Square value={squares[24]} title={name[24]} background={board[24]} onSquareClick={() => handleClick(24)} />
+        <Square value={squares[20]} title={name[20]} background={board[20]} selection={marker} onSquareClick={() => handleClick(20)} />
+        <Square value={squares[21]} title={name[21]} background={board[21]} selection={marker} onSquareClick={() => handleClick(21)} />
+        <Square value={squares[22]} title={name[22]} background={board[22]} selection={marker} onSquareClick={() => handleClick(22)} />
+        <Square value={squares[23]} title={name[23]} background={board[23]} selection={marker} onSquareClick={() => handleClick(23)} />
+        <Square value={squares[24]} title={name[24]} background={board[24]} selection={marker} onSquareClick={() => handleClick(24)} />
       </div>
       <div className="status">{status}</div>
       <Button className="shuffle" onButtonClick={() => populateImages()}>Shuffle</Button>
@@ -227,9 +170,6 @@ export default function Board() {
           <button><li onClick={() => markerPick(5)}>Pear</li></button>
         </ul>
       </div>
-      {/* <img src={marker} alt="game marker" /> */}
-      {/* <img src={orangeImg} alt="game marker" /> */}
-      <Marker selection={marker} />
     </div>
   );
 }
