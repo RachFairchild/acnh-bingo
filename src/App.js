b30e8ab5fd35464af57c42d48eb7e7648c685ba8
Rachel Fairchild <rfair009@gmail.com>
2023-05-23 13:03:23 -0400

    Test to see if names generate on deploy

--- src/App.js
+++ src/App.js
@@ -138,6 +138,7 @@ export default function Board() {
   }
 
   async function villagerName(villagerID) {
+    // Add catch
     const response = await fetch(`http://acnhapi.com/v1/villagers/${villagerID}`);
     let data = await response.json();
     const currentVillager = data.name["name-USen"];
